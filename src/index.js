f07b5805b8b3f14542245875a46e48283caea043
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-02-27 00:20:11 -0300

    relembrando conceitos de crud

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
 const express = require("express");
+const {v4} = require('uuid');
 
 const app = express();
-app.get('/',  (req, res)=> {
-  return res.json({
-    message: 'chegou no get'
-  })
+app.use(express.json())
+
+const clients = [];
+app.post('/cadastro', (req, res)=>{
+  const { name, age } = req.body;
+  const id = v4()
+
+  const client = {
+    id,
+    name,
+    age
+  }
+
+   clients.push(client)
+   return res.json(client)
 })
 
-app.post('/', (req, res)=>{
-  return res.json({
-    message: 'chegou no post'
-  })
+app.get('/cadastro',  (req, res)=> {
+  return res.json(clients)
 })
 
-app.put('/', (req, res)=>{
-  return res.json({
-    message: 'chegou no put'
-  })
+app.put('/cadastro/:id', (req, res)=>{
+  const {id} = req.params;
+  const{name, age} = req.body;
+
+  const client = clients.filter(item => item.id = id);
+  if(!client) {
+    return res.status(400).json({
+      error: 'nao encontrado'
+    })
+  }
+
+  const clientAtt = {
+    id,
+    name,
+    age
+  }
+
+  return res.json(clientAtt)
 })
 
-app.delete('/', (req, res)=>{
-  return res.json({
-    message: 'chegou no delete'
-  })
+app.delete('/cadastro/:id', (req, res)=>{
+  const {id} = req.params;
+  const client = clients.filter(item => item.id !== id)
+  return res.status(204).json(client)
 })
 
-app.listen(3003, () => {
+app.listen(3005, () => {
   console.log('server subiu')
 });
