babfd00551af6b5f55ecf43b1eeec6f2fe2f869b
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-02-28 11:55:06 -0300

    testando script tsc

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.get('/', (req, res) => {
+ return res.json({
+   message: 'Olá'
+ })
+})
+
+app.listen(3333, ()=>{
+  console.log('Server is running on port 3333!')
+})
