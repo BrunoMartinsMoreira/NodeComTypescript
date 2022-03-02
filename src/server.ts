c49ce1b758c8fd25ea06c812d44262270bb569ce
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-01 22:07:07 -0300

    arquivos deletados para reestruturar a aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import { routes } from './Routes';
+import { router } from './routes';
 
 const app = express();
 
-app.use(routes);
+app.use(router);
 
 app.listen(3333, () => {
   console.log('Server is running on port 3333')
