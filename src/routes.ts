11285bfef52406b9f719779d2a5afe0ec1372514
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-05 22:14:47 -0300

    criação das rotas de users

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import { ItemsController } from "./controllers/ItemsController";
 import { LocationsController } from "./controllers/LocationsController";
+import { UsersController } from "./controllers/usersController";
 
 const router = Router();
 
 const itemsController = new ItemsController();
 const locationsController = new LocationsController();
+const usersController = new UsersController();
 
 router.get('/items', itemsController.list);
 router.post('/locations', locationsController.create);
 router.get('/:id', locationsController.findOneLocation);
 router.get('/location',locationsController.findAllLocations);
+router.post('/users', usersController.create);
+router.get('/users', usersController.listUsers);
 
 export {router}
