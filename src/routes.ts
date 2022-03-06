bb0445154fe7f8ae11402796ab828fa93f10cce3
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-05 23:18:04 -0300

    create session controller

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { ItemsController } from "./controllers/ItemsController";
 import { LocationsController } from "./controllers/LocationsController";
 import { UsersController } from "./controllers/UsersController";
+import { SessionsController } from "./controllers/SessionsController";
 
 const router = Router();
 
 const itemsController = new ItemsController();
 const locationsController = new LocationsController();
 const usersController = new UsersController();
+const sessionsController = new SessionsController();
 
 router.get('/items', itemsController.list);
 router.post('/locations', locationsController.create);
@@ -15,5 +17,6 @@ router.get('/:id', locationsController.findOneLocation);
 router.get('/locations',locationsController.findAllLocations);
 router.post('/users', usersController.create);
 router.get('/users', usersController.listUsers);
+router.post('/user/login', sessionsController.login);
 
 export {router}
