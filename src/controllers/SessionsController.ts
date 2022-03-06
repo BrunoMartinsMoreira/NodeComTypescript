0b6de4b134b6dc0387b528f65623658531f246f9
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-05 23:17:39 -0300

    create sessiont controller

---
+++ src/controllers/SessionsController.ts
@@ -0,0 +1,45 @@
+import { connection as knex } from "../database/connection";
+import {Request, Response} from 'express';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+interface Iuser{
+  email: string,
+  password: string
+}
+
+class SessionsController {
+  async login(req:Request, res:Response){
+    const {email, password}: Iuser = req.body;
+
+    const user = await knex('users').where('email', email).first();
+
+    if(!user){
+      return res.status(418).json({
+        error: 'username or password is invalid.'
+      })
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if(!isPasswordValid){
+      return res.status(418).json({
+        error: 'username or password is invalid.'
+      })
+    }
+
+    const token = sign(
+      {},
+      process.env.SIGNATURE_TOKEN,
+      {
+        subject:user.id,
+        expiresIn: "1d"
+      }
+    )
+
+    return res.json(token);
+    
+  }
+}
+
+export {SessionsController};
