2535260e607a70b9323fcbae5550315e273ce026
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-03-06 22:57:51 -0300

    autenticando rotas

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { ItemsController } from "./controllers/ItemsController";
 import { LocationsController } from "./controllers/LocationsController";
 import { UsersController } from "./controllers/UsersController";
 import { SessionsController } from "./controllers/SessionsController";
+import { isUserAuthenticated } from "./middlewares/isUserAuthenticated";
 
 const router = Router();
 
@@ -11,12 +12,12 @@ const locationsController = new LocationsController();
 const usersController = new UsersController();
 const sessionsController = new SessionsController();
 
-router.get('/items', itemsController.list);
-router.post('/locations', locationsController.create);
-router.get('/:id', locationsController.findOneLocation);
-router.get('/locations',locationsController.findAllLocations);
+router.get('/items', isUserAuthenticated, itemsController.list);
+router.post('/locations', isUserAuthenticated, locationsController.create);
+router.get('/:id', isUserAuthenticated, locationsController.findOneLocation);
+router.get('/locations', isUserAuthenticated, locationsController.findAllLocations);
 router.post('/users', usersController.create);
-router.get('/users', usersController.listUsers);
+router.get('/users',isUserAuthenticated, usersController.listUsers);
 router.post('/user/login', sessionsController.login);
 
 export {router}
